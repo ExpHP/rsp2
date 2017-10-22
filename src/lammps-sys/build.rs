fe2ef4010f2f53e44158489d6ed35a0de0c4ea32
Michael Lamparski <diagonaldevice@gmail.com>
2017-10-22 10:23:08 -0400

    bindgen for lammps

--- src/lammps-sys/build.rs
+++ src/lammps-sys/build.rs
@@ -1,4 +1,18 @@
+extern crate bindgen;
+use std::env;
+use std::path::PathBuf;
+
 fn main() {
+
     // Tell cargo to tell rustc to link the system lammps shared library.
     println!("cargo:rustc-link-lib=lammps");
+
+    // Generate bindings at build time.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    ::bindgen::Builder::default()
+        .header("src/wrapper.h")
+        .generate()
+        .expect("Unable to generate bindings")
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
 }
