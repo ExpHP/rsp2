4ecfc5af9fbce6e44ac3818db05899e9c933e355
Michael Lamparski <diagonaldevice@gmail.com>
2018-05-11 16:06:02 -0400

    get rid of sparse-matrix
    WELP THAT WAS A GOOD RUN

--- src/util/sparse/mat/mod.rs
+++
@@ -1,7 +0,0 @@
-//! Sparse matrices in various formats.
-
-pub mod coo;
-pub mod cs;
-
-pub use self::coo::CooMat;
-pub use self::cs::{CscMat, CsrMat};
--- src/util/sparse/math/mod.rs
+++
@@ -1,8 +0,0 @@
-mod ops;
-pub use self::ops::*;
-
-mod decomp;
-pub use self::decomp::*;
-
-mod mat_mul;
-pub use self::mat_mul::*;
