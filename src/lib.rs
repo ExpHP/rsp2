f0659c71655441faefb9e45d16ec33c7cb62d729
Michael Lamparski <diagonaldevice@gmail.com>
2018-09-04 16:43:36 -0400

    apparently I forgot to add build.rs and src/lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+extern crate rsp2_tasks;
+
+pub mod version {
+    use ::rsp2_tasks::VersionInfo;
+
+    pub fn get() -> VersionInfo {
+        VersionInfo {
+            short_sha: env!("VERGEN_SHA_SHORT"),
+            commit_date: env!("VERGEN_COMMIT_DATE"),
+        }
+    }
+}
