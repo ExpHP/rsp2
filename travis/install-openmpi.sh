b54316b316e78c70ffa6976a22a49c5bf673d590
Michael Lamparski <diagonaldevice@gmail.com>
2018-09-06 10:55:22 -0400

    the file having contents might help even more

---
+++ travis/install-openmpi.sh
@@ -0,0 +1,7 @@
+cd builds || exit 1
+wget https://download.open-mpi.org/release/open-mpi/v${OPENMPI_VERSION_SHORT}/openmpi-${OPENMPI_VERSION}.tar.bz2 || exit 1
+tar -xf openmpi-${OPENMPI_VERSION}.tar.bz2 || exit 1
+cd openmpi-$OPENMPI_VERSION || exit 1
+
+./configure --prefix=$HOME/deps || exit 1
+make install || exit 1
