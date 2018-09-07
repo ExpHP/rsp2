a8b4e36588e553ca1701619c4c3cb44bc4e62d19
Michael Lamparski <diagonaldevice@gmail.com>
2018-09-07 12:17:09 -0400

    kill travis helper script

--- travis/install-openmpi.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd $HOME/builds || exit 1
-wget -q https://download.open-mpi.org/release/open-mpi/v${OPENMPI_VERSION_SHORT}/openmpi-${OPENMPI_VERSION}.tar.bz2 || exit 1
-tar -xf openmpi-${OPENMPI_VERSION}.tar.bz2 || exit 1
-cd openmpi-$OPENMPI_VERSION || exit 1
-
-./configure --prefix=$HOME/deps || exit 1
-make -j2 install >install.log || exit 1
