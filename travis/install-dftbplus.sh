2c286d37db0c8c8e5aca752b21afe3ba032a0367
Michael Lamparski <diagonaldevice@gmail.com>
2019-05-20 17:25:02 -0400

    fix paths

--- travis/install-dftbplus.sh
+++ travis/install-dftbplus.sh
@@ -6,7 +6,7 @@ CACHE=$HOME/cache/dftbplus-${DFTBPLUS_REV}
 
 [[ -e $CACHE ]] || {
     PREFIX=$CACHE
-    DATAROOTDIR=$PREFIX/usr/share
+    DATAROOTDIR=$PREFIX/share
     DATADIR=$DATAROOTDIR
     INCLUDEDIR=$PREFIX/include
     LIBDIR=$PREFIX/lib
@@ -31,7 +31,7 @@ CACHE=$HOME/cache/dftbplus-${DFTBPLUS_REV}
     # install_api is a bit messy and incomplete.
     rm "$INCLUDEDIR"/*.mod # don't need all these fortran modules
     install -Dm644 LICENSE "$DATAROOTDIR/licenses/dftbplus/LICENSE"
-    install -Dm644 api/mm/dftbplus.h "$PREFIX/dftbplus.h"
+    install -Dm644 api/mm/dftbplus.h "$INCLUDEDIR/dftbplus.h"
     install -D _build/external/xmlf90/libxmlf90.a "$LIBDIR/libxmlf90.a"
 
     mkdir -p "$LIBDIR/pkgconfig"
