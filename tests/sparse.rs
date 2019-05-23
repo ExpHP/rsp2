5816a63298ae537b8d0f17b6e1c8ece9d4dc43d3
Michael Lamparski <diagonaldevice@gmail.com>
2019-05-23 17:29:09 -0400

    add dynmat_rust test

--- tests/sparse.rs
+++ tests/sparse.rs
@@ -2,11 +2,32 @@ use ::rsp2_integration_test::{CliTest, filetypes, resource, cli_test, Result};
 
 #[ignore] // This test is expensive; use `cargo test -- --ignored` to run it!
 #[test]
-fn simple_test() -> Result<()> {
+fn dynmat_lammps() -> Result<()> {
     let env = cli_test::Environment::init();
     CliTest::cargo_binary(&env, "rsp2")
         .arg("-c").arg(resource("defaults.yaml"))
         .arg("-c").arg(resource("sparse.yaml"))
+        .arg(resource("simple.vasp"))
+        .arg("-o").arg("out")
+        .check_file::<filetypes::Dynmat>(
+            "out/gamma-dynmat.npz".as_ref(),
+            resource("sparse-out/gamma-dynmat.npz").as_ref(),
+            filetypes::DynmatTolerances {
+                rel_tol: 1e-9,
+                abs_tol: 1e-9,
+            },
+        )
+        .run()
+}
+
+// Test custom DispFns on the potentials implemented in Rust
+#[ignore] // This test is expensive; use `cargo test -- --ignored` to run it!
+#[test]
+fn dynmat_rust() -> Result<()> {
+    let env = cli_test::Environment::init();
+    CliTest::cargo_binary(&env, "rsp2")
+        .arg("-c").arg(resource("defaults.yaml"))
+        .arg("-c").arg(resource("sparse-rust.yaml"))
         .arg(resource("simple.vasp"))
         .arg("-o").arg("out")
         .check_file::<filetypes::Dynmat>(
