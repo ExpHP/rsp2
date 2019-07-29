fix sign error(?) in unfolding
This finally appears to produce completely correct output
and is enough for me to start running computations again.
Though I am perplexed; I'm pretty sure this should be a plus sign,
so the true sign error must lie somewhere else. (possibly multiple!)

scripts/unfold.py
@@ -1028,7 +1028,10 @@ def unfold_one(
     gpoint_probs = []
     for g in gpoint_sfracs:
         # SBZ kpoint dot r for every r
-        k_dot_rs = (kpoint_sfrac + g) @ translation_sfracs.T
+        #
+        # FIXME: '- g' doesn't seem right here, but it's what produces the correct behavior.
+        #        There may be another sign error somewhere that this cancels out with?
+        k_dot_rs = (kpoint_sfrac - g) @ translation_sfracs.T
         phases = np.exp(-2j * np.pi * k_dot_rs)
 
         prob = sum(inner_prods * phases) / sizes['quotient']