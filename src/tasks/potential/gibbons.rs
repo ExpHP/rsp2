beae784c3b78512253ac07b15e37c71f1c03b434
Michael Lamparski <diagonaldevice@gmail.com>
2022-09-13 15:49:05 -0400

    add license to gibbons.rs

--- src/tasks/potential/gibbons.rs
+++ src/tasks/potential/gibbons.rs
@@ -1,3 +1,17 @@
+/* ********************************************************************** **
+**  This file is part of rsp2.                                            **
+**                                                                        **
+**  rsp2 is free software: you can redistribute it and/or modify it under **
+**  the terms of the GNU General Public License as published by the Free  **
+**  Software Foundation, either version 3 of the License, or (at your     **
+**  option) any later version.                                            **
+**                                                                        **
+**      http://www.gnu.org/licenses/                                      **
+**                                                                        **
+** Do note that, while the whole of rsp2 is licensed under the GPL, many  **
+** parts of it are licensed under more permissive terms.                  **
+** ********************************************************************** */
+
 //! Wyatt Gibbons' external potential. (2022)
 
 use super::{DynCloneDetail, PotentialBuilder, DiffFn, DispFn, BondDiffFn};
